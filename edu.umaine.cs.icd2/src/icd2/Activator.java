17f27a23f6c40b14012a5155d142c9ef5aeddfaa
markroyer <mark.e.royer@gmail.com>
2016-09-01 13:10:45 -0400

    Add workspace timestamp

--- edu.umaine.cs.icd2/src/icd2/Activator.java
+++ edu.umaine.cs.icd2/src/icd2/Activator.java
@@ -1,6 +1,14 @@
 package icd2;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.commons.math3.util.Pair;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.preferences.ConfigurationScope;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.ui.internal.workbench.E4Workbench;
 import org.eclipse.jface.dialogs.MessageDialog;
@@ -10,6 +18,7 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.service.prefs.Preferences;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,7 +32,7 @@ public class Activator implements BundleActivator {
 
 	private static final Logger logger = LoggerFactory
 			.getLogger(Activator.class);
-	
+
 	static BundleContext getContext() {
 		return context;
 	}
@@ -37,7 +46,7 @@ public class Activator implements BundleActivator {
 	 */
 	@Override
 	public void start(BundleContext bundleContext) throws Exception {
-		
+
 		Activator.context = bundleContext;
 
 		IEclipseContext ctx = E4Workbench.getServiceContext();
@@ -51,16 +60,39 @@ public class Activator implements BundleActivator {
 		String ecs = System.getProperty("eclipse.commands");
 
 		boolean found = false;
-		for (String s: ecs.split("\\s+")) {
+		for (String s : ecs.split("\\s+")) {
 			if ("-testproperties".equals(s)) {
 				found = true;
 				break;
 			}
 		}
-		
+
 		if (!found) {
 
-			PickWorkspaceDialog pwd = new PickWorkspaceDialog(shell);
+			Preferences prefs = ConfigurationScope.INSTANCE
+					.getNode("edu.umaine.cs.icd2");
+
+			Preferences prefWorkspaces = prefs.node("workspaces");
+
+			String[] workspaces = prefWorkspaces.keys();
+			List<Pair<String, String>> wsAccessTimes = new ArrayList<>(
+					workspaces.length);
+			Arrays.stream(workspaces).forEachOrdered(
+					a -> wsAccessTimes.add(new Pair<>(a, prefWorkspaces.get(a,
+							String.valueOf(System.currentTimeMillis())))));
+
+			Collections.sort(wsAccessTimes,
+					(a, b) -> b.getSecond().compareTo(a.getSecond()));
+
+			if (workspaces.length == 0) {
+				wsAccessTimes.add(new Pair<String, String>(System.getProperty("user.home")
+						+ File.separator + "icdWorkspace", "never used"));
+			}
+
+			logger.debug("Known workspaces = {}", (Object)workspaces);
+
+			PickWorkspaceDialog pwd = new PickWorkspaceDialog(shell,
+					wsAccessTimes);
 			int pick = pwd.open();
 
 			Workspace workspace = null;
@@ -76,12 +108,17 @@ public class Activator implements BundleActivator {
 			} else {
 				workspace = pwd.getSelectedWorkspace();
 				instanceLoc.set(workspace.getFile().toURI().toURL(), false);
+				
+				// Remember the location for next time
+				prefWorkspaces.put(workspace.getFile().getAbsolutePath(), String.valueOf(System.currentTimeMillis()));
+				
 			}
 
 			ctx.set(Workspace.class, workspace);
 
 		} else {
-			logger.info("testproperties flag found, so assume testing and don't allow workspace to be chosen...");
+			logger.info(
+					"testproperties flag found, so assume testing and don't allow workspace to be chosen...");
 		}
 
 	}
@@ -94,13 +131,14 @@ public class Activator implements BundleActivator {
 	 */
 	@Override
 	public void stop(BundleContext bundleContext) throws Exception {
-		
-//		IEclipseContext ctx = E4Workbench.getServiceContext();
-//		IEclipsePreferences prefs = InstanceScope.INSTANCE.getNode("edu.umaine.cs.icd2");
-//		
-//		logger.info("Finishing open projects are {}", ctx.get(PreferenceKeys.OPEN_PROJECTS));
-		
-		
+
+		// IEclipseContext ctx = E4Workbench.getServiceContext();
+		// IEclipsePreferences prefs =
+		// InstanceScope.INSTANCE.getNode("edu.umaine.cs.icd2");
+		//
+		// logger.info("Finishing open projects are {}",
+		// ctx.get(PreferenceKeys.OPEN_PROJECTS));
+
 		Activator.context = null;
 	}
 
--- edu.umaine.cs.icd2/src/icd2/widgets/PickWorkspaceDialog.java
+++ edu.umaine.cs.icd2/src/icd2/widgets/PickWorkspaceDialog.java
@@ -1,16 +1,24 @@
 package icd2.widgets;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Optional;
 
+import org.apache.commons.math3.util.Pair;
 import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ComboViewer;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.DirectoryDialog;
@@ -28,13 +36,18 @@ import icd2.model.Workspace;
  */
 public class PickWorkspaceDialog extends TitleAreaDialog {
 
-	private static final Logger logger = LoggerFactory.getLogger(PickWorkspaceDialog.class);
+	private static final Logger logger = LoggerFactory
+			.getLogger(PickWorkspaceDialog.class);
 
-	public PickWorkspaceDialog(Shell parentShell) {
+	private List<Pair<String, String>> wsAccessTimes;
+
+	public PickWorkspaceDialog(Shell parentShell,
+			List<Pair<String, String>> wsAccessTimes) {
 		super(parentShell);
+		this.wsAccessTimes = wsAccessTimes;
 	}
 
-	Combo workspaceCombo;
+	ComboViewer workspaceCombo;
 
 	String selectedPath;
 
@@ -43,7 +56,8 @@ public class PickWorkspaceDialog extends TitleAreaDialog {
 	@Override
 	protected Control createDialogArea(Composite parent) {
 		setTitle("Select a workspace");
-		setMessage("Select a workspace to store related dating files.\nThis is simply a folder that will contain projects and core data.");
+		setMessage(
+				"Select a workspace to store related dating files.\nThis is simply a folder that will contain projects and core data.");
 
 		try {
 			Composite inner = new Composite(parent, SWT.NONE);
@@ -52,20 +66,45 @@ public class PickWorkspaceDialog extends TitleAreaDialog {
 
 			// label on left
 			CLabel label = new CLabel(inner, SWT.NONE);
-			label.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1));
+			label.setLayoutData(
+					new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1));
 			label.setText("Workspace Root Path");
 
 			// combo in middle
-			workspaceCombo = new Combo(inner, SWT.BORDER);
-			workspaceCombo.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL));
+			workspaceCombo = new ComboViewer(inner,
+					SWT.READ_ONLY);
+			// workspaceCombo = new Combo(inner, SWT.BORDER);
+			// workspaceCombo.setLayoutData(new
+			// GridData(GridData.GRAB_HORIZONTAL));
 
-			List<String> _lastUsedWorkspaces = new ArrayList<>(); // TODO come
-																	// back to
-																	// this
-			_lastUsedWorkspaces.add(System.getProperty("user.home") + File.separator + "icdWorkspace");
-			for (String last : _lastUsedWorkspaces)
-				workspaceCombo.add(last);
-			workspaceCombo.select(0);
+			// for (Pair<String,String> last : wsAccessTimes)
+			// workspaceCombo.add(last.getFirst() + " (" + last.getSecond() +
+			// ")");
+			// workspaceCombo.select(0);
+
+			workspaceCombo
+					.setContentProvider(ArrayContentProvider.getInstance());
+			workspaceCombo.setInput(wsAccessTimes);
+
+			workspaceCombo.setLabelProvider(new LabelProvider() {
+				SimpleDateFormat df = new SimpleDateFormat();
+				@Override
+				public String getText(Object element) {
+					Pair<String,String> p = (Pair)element;
+					String date = p.getSecond();
+					try {
+						date = df.parse(date).toString();
+					} catch (ParseException e) {
+						// Unable to format
+					}
+					
+					return p.getFirst() + " (" + date + ")";
+					
+					
+				}
+			});
+			
+			workspaceCombo.setSelection(new StructuredSelection(wsAccessTimes.get(0)), true);
 
 			// browse button on right
 			Button browse = new Button(inner, SWT.PUSH);
@@ -80,15 +119,31 @@ public class PickWorkspaceDialog extends TitleAreaDialog {
 					dd.setText("Select Workspace");
 					dd.setMessage("Select a folder for the workspace.");
 					String pick = dd.open();
-					if (pick != null)
-						workspaceCombo.setText(pick);
+					if (pick != null) {
+//						workspaceCombo.setText(pick);
+						Optional<Pair<String, String>> match = wsAccessTimes.stream().filter(a-> a.getFirst().equals(pick)).findAny();
+						ISelection selection;
+						if (match.isPresent())
+							selection = new StructuredSelection(match.get());
+						else {
+							Pair<String, String> newPair = new Pair<>(pick, "never used");
+//							wsAccessTimes.add(0, newPair);
+							workspaceCombo.insert(newPair,0);
+							selection = new StructuredSelection(newPair);
+						}
+							
+						workspaceCombo.setSelection(selection);
+					}
 				}
 			});
 
 			// checkbox below
-			Button _RememberWorkspaceButton = new Button(inner, SWT.CHECK);
-			_RememberWorkspaceButton.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 3, 1));
-			_RememberWorkspaceButton.setText("Remember workspace");
+			Button rememberWorkspaceButton = new Button(inner, SWT.CHECK);
+			rememberWorkspaceButton.setLayoutData(
+					new GridData(SWT.LEFT, SWT.CENTER, true, false, 3, 1));
+			rememberWorkspaceButton.setText("Remember workspace");
+			rememberWorkspaceButton.setToolTipText(
+					"Selecting this option will make it so that you are not prompted for the workspace location again");
 
 			inner.pack();
 
@@ -102,11 +157,19 @@ public class PickWorkspaceDialog extends TitleAreaDialog {
 	@Override
 	protected void okPressed() {
 
-		selectedPath = workspaceCombo.getText();
+		ISelection iSelect = workspaceCombo.getSelection();
 
-		if (isValid(selectedPath)) {
-			selection = new Setup().getWorkspace(new File(selectedPath));
+		if (iSelect != null) {
+		
+			selectedPath = ((Pair<String,String>)((StructuredSelection)iSelect).getFirstElement()).getFirst();
+			
+			if (isValid(selectedPath)) {
+				selection = new Setup().getWorkspace(new File(selectedPath));
+			}
 		}
+		
+		
+		
 
 		super.okPressed();
 	}
