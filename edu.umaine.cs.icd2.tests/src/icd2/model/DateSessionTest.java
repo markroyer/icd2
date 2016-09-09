fbf470228442afcf00cd3bdba729c4c982a0ef64
markroyer <mark.e.royer@gmail.com>
2016-09-08 22:58:40 -0400

    Add initial DateSessionTest

--- edu.umaine.cs.icd2.tests/src/icd2/model/DateSessionTest.java
+++ edu.umaine.cs.icd2.tests/src/icd2/model/DateSessionTest.java
@@ -3,10 +3,32 @@
  */
 package icd2.model;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * @author Mark Royer
  *
  */
 public class DateSessionTest {
+
+	DateSession dateSession;
+
+	@Before
+	public void setUp() {
+		dateSession = new DateSession(null, "Test", 2016);
+	}
+
+	@Test
+	public void testAddYearDepth() {
+
+		dateSession.addYearDepth(2015, .1);
+		
+		assertEquals(2016, dateSession.getYear(0));
+		assertEquals(2015, dateSession.getYear(1));
+
+	}
 
 }
