846065f084df98be940f3c692f6492f9b184d8b2
markroyer <mark.e.royer@gmail.com>
2016-09-20 10:43:00 -0400

    Add first two remove date tests

--- edu.umaine.cs.icd2.tests/src/icd2/model/DateSessionTest.java
+++ edu.umaine.cs.icd2.tests/src/icd2/model/DateSessionTest.java
@@ -8,6 +8,8 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
+import icd2.model.DateSession.DateSessionException;
+
 /**
  * @author Mark Royer
  *
@@ -29,6 +31,21 @@ public class DateSessionTest {
 		assertEquals(2016, dateSession.getYear(0));
 		assertEquals(2015, dateSession.getYear(1));
 
+	}
+	
+	@Test(expected=DateSessionException.class)
+	public void testRemoveYearDepthTopYear() throws DateSessionException {
+		// Remove the core top depth aka the first depth
+		dateSession.removeYearDepth(2016);
+	}
+	
+	@Test
+	public void testRemoveYearDepth() throws Exception {
+		
+		// Insert and remove 1
+		dateSession.insertDepth(.1);
+		dateSession.removeYearDepth(2015);
+		assertEquals(2016, dateSession.getYear(0));
 	}
 
 }
