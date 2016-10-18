1d6e90f69ac541416eae673705fb90eecca4ea51
markroyer <mark.e.royer@gmail.com>
2016-10-18 17:10:26 -0400

    Add javadoc description

--- edu.umaine.cs.icd2/src/icd2/model/ModelKey.java
+++ edu.umaine.cs.icd2/src/icd2/model/ModelKey.java
@@ -4,22 +4,29 @@
 package icd2.model;
 
 /**
+ * {@link ModelKey}s represent a path to information about a model object. Most
+ * commonly these keys are used to refer to the location of core data. For
+ * example, the model key "COREID/ELEMENTID" would uniquely identify a element
+ * data for the ELEMENTID of the core COREID.
+ * 
  * @author Mark Royer
  *
  */
-public class ModelKey<T> implements ModelObject<ModelKey<T>, ModelObject<?,?>> {
+public class ModelKey<T>
+		implements ModelObject<ModelKey<T>, ModelObject<?, ?>> {
 
 	private T key;
-	
+
 	private ModelObject<?, ?> parent;
-	
+
 	/**
-	 * @param key (Not null)
+	 * @param key
+	 *            (Not null)
 	 */
 	public ModelKey(T key) {
 		this.key = key;
 	}
-	
+
 	@Override
 	public void setParent(ModelObject<?, ?> parent) {
 		this.parent = parent;
@@ -45,6 +52,4 @@ public class ModelKey<T> implements ModelObject<ModelKey<T>, ModelObject<?,?>> {
 		return getName();
 	}
 
-	
-	
 }
