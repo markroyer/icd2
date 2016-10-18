c201b556c05bd719b9187fdf825a5401fc98d8ba
markroyer <mark.e.royer@gmail.com>
2016-10-18 14:37:41 -0400

    Add export session data handler

---
+++ edu.umaine.cs.icd2/src/icd2/handlers/ExportSessionDataHandler.java
@@ -0,0 +1,19 @@
+
+package icd2.handlers;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ExportSessionDataHandler {
+
+	@SuppressWarnings("unused")
+	private static final Logger logger = LoggerFactory
+			.getLogger(ExportSessionDataHandler.class);
+
+	@Execute
+	public void execute() {
+
+	}
+
+}
