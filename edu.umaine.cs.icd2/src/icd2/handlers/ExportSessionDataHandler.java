28faef0a8c8bd60111ad35b735b8d5944904b659
markroyer <mark.e.royer@gmail.com>
2016-10-20 10:46:31 -0400

    Add export all data spline interpolation for csv files

--- edu.umaine.cs.icd2/src/icd2/handlers/ExportSessionDataHandler.java
+++ edu.umaine.cs.icd2/src/icd2/handlers/ExportSessionDataHandler.java
@@ -7,8 +7,8 @@ import static icd2.widgets.FileDialogExport.isRecognizedFile;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import javax.inject.Named;
 
@@ -24,10 +24,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import edu.umaine.cs.h5.H5ConnectorException;
+import icd2.model.Core;
+import icd2.model.CoreData;
 import icd2.model.CoreModelConstants;
 import icd2.model.DateSession;
 import icd2.model.ObjectNotFound;
 import icd2.model.Plot;
+import icd2.model.Sample;
+import icd2.util.DataFileException;
 import icd2.widgets.FileDialogExport;
 
 public class ExportSessionDataHandler {
@@ -66,8 +70,8 @@ public class ExportSessionDataHandler {
 			if (".h5".equalsIgnoreCase(fileExt)
 					|| ".hdf".equalsIgnoreCase(fileExt))
 				writeHDFFile(new File(filePath), ds);
-		} catch (FileNotFoundException | H5ConnectorException
-				| ObjectNotFound e) {
+		} catch (FileNotFoundException | H5ConnectorException | ObjectNotFound
+				| DataFileException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -86,51 +90,72 @@ public class ExportSessionDataHandler {
 	 *            (Not null)
 	 * @return Interpolated year data
 	 */
-	private double[] interpolateYearData(DateSession ds,
+	private List<Double> interpolateYearData(DateSession ds,
 			List<Double> depthData) {
 		double[] dr = ds.getDepthArray();
 		double[] yr = ds.getYearArray();
 		SplineInterpolator si = new SplineInterpolator();
 		PolynomialSplineFunction spf = si.interpolate(dr, yr);
 
-		double[] result = new double[depthData.size()];
+		List<Double> result = new ArrayList<Double>(depthData.size());
 
-		for (int i = 0; i < depthData.size(); i++) {
-			result[i] = spf.value(depthData.get(i));
+		int i = 0;
+
+		for (; i < depthData.size() && (depthData.get(i) == null
+				|| spf.isValidPoint(depthData.get(i))); i++) {
+			result.add(depthData.get(i) == null ? null
+					: spf.value(depthData.get(i)));
+		}
+
+		if (i < depthData.size()) {
+			logger.warn("Not enough dates.  Did not interpolate {} values.",
+					depthData.size() - i);
 		}
 
 		return result;
 	}
 
 	public void writeCSVFile(File file, DateSession dateSession)
-			throws FileNotFoundException, ObjectNotFound {
+			throws FileNotFoundException, ObjectNotFound, DataFileException {
 
 		logger.debug("Need to implement export CSV File");
-		
+
 		// Start of csv file code is below
 
-		// PrintWriter out = new PrintWriter(file);
-		//
-		// Plot plot = dateSession.getParent().getChart().getPlots()[0][0];
-		//
-		// out.println(plot.getRangeValues().stream()
-		// .map(e -> e.getName().split("/")[1])
-		// .reduce((e1, e2) -> e1 + ", " + e2).get());
-		//
-		// double[] depthArray = dateSession.getDepthArray();
-		// double[] yearArray = dateSession.getYearArray();
-		//
-		// double[] interpYearData = interpolateYearData(dateSession,
-		// plot.getXData().stream().map(e -> e.doubleValue())
-		// .collect(Collectors.toList()));
-		//
-		// out.println("Depth, Year");
-		// for (int i = 0; i < yearArray.length; i++) {
-		// out.printf("%f,%.0f\n", depthArray[i], yearArray[i]);
-		// }
-		//
-		// out.flush();
-		// out.close();
+		PrintWriter out = new PrintWriter(file);
+
+		Plot plot = dateSession.getParent().getChart().getPlots()[0][0];
+
+		CoreData cd = dateSession.getParent().getParent().getCoreData();
+
+		Core core = cd
+				.lookupSample(plot.getDomainValues().get(0).getValuesKey())
+				.getParent();
+
+		List<Sample> samples = core.getSamples();
+
+		List<Double> resampleYear = interpolateYearData(dateSession,
+				plot.getXData());
+		int sSize = resampleYear.size();
+
+		// Write header info
+		out.printf("%s", "Year");
+		for (int i = 0; i < samples.size(); i++) {
+			out.printf(", %s", samples.get(i).getName());
+		}
+		out.println();
+
+		for (int i = 0; i < sSize; i++) {
+			out.printf("%f", resampleYear.get(i));
+			for (int j = 0; j < samples.size(); j++) {
+				out.printf(", %f",
+						samples.get(j).getValues().get(i).doubleValue());
+			}
+			out.println();
+		}
+
+		out.flush();
+		out.close();
 	}
 
 	public void writeHDFFile(File file, DateSession dateSession)
